3e00b5a0881c1efcbe4e3f04cda8df1a4147292f
FardeenB13 <fardeenbilal2932@gmail.com>
2025-04-08 14:22:57 -0400

    Sidebar with filters for listings and location

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import Sidebar from '../components/sidebar';
 
-const App: React.FC = () => {
+const Home: React.FC = () => {
+
   const handleSearch = (query: string) => {
     console.log('Searching for:', query);
     alert(`You searched for: ${query}`);
   };
+const handleCategoryChange = (categories: string[]) => {
+    console.log('Selected Categories:', categories);
+  };
 
+  const handleCountyChange = (county: string) => {
+    console.log('Selected County:', county);
+  };
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Explore Local Art</h1>
       <SearchBar onSearch={handleSearch} />
+      <Sidebar onCategoryChange={handleCategoryChange} onCountyChange={handleCountyChange} />
     </div>
   );
 };
 
-export default App;
+export default Home;
