24709e255059f23f274e481627c81f27faff76e9
hsollas22 <haileysollas@gmail.com>
2025-04-29 00:47:13 -0400

    Modified add listing widget to allow text input for county and select any of the 50 states instead of only NJ

--- Frontend/src/components/SellerProfileInfo/AddListingModal.tsx
+++ Frontend/src/components/SellerProfileInfo/AddListingModal.tsx
@@ -194,37 +194,13 @@ const AddListingModal: React.FC<AddListingModalProps> = ({
               <option value="digital">Other</option>
             </Select>
 
-            <Label>Location</Label>
-            <Select
+            <Label>County</Label>
+            <Input
+              type="text"
               value={county}
               onChange={(e) => setCounty(e.target.value)}
               required
-            >
-              <option value="" disabled>
-                Select Location
-              </option>
-              <option value="atlantic">Atlantic</option>
-              <option value="bergen">Bergen</option>
-              <option value="burlington">Burlington</option>
-              <option value="camden">Camden</option>
-              <option value="cape-may">Cape May</option>
-              <option value="cumberland">Cumberland</option>
-              <option value="essex">Essex</option>
-              <option value="gloucester">Gloucester</option>
-              <option value="hudson">Hudson</option>
-              <option value="hunterdon">Hunterdon</option>
-              <option value="mercer">Mercer</option>
-              <option value="middlesex">Middlesex</option>
-              <option value="monmouth">Monmouth</option>
-              <option value="morris">Morris</option>
-              <option value="ocean">Ocean</option>
-              <option value="passaic">Passaic</option>
-              <option value="salem">Salem</option>
-              <option value="somerset">Somerset</option>
-              <option value="sussex">Sussex</option>
-              <option value="union">Union</option>
-              <option value="warren">Warren</option>
-            </Select>
+            />
             
             <Label>State</Label>
             <Select
@@ -232,7 +208,59 @@ const AddListingModal: React.FC<AddListingModalProps> = ({
               onChange={(e) => setState(e.target.value)}
               required
             >
+              <option value="" disabled>
+              Select State
+              </option>
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
               <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
             </Select>
 
             <Label>Upload Image:</Label>
